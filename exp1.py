501f01964bbf1b202dc19f89e5e0a496e531e943
AI LAB <25cseaiml007.suhanakarji@giet.edu>
2026-07-17 09:11:26 +0530

    Add my file

--- exp1.py
+++ exp1.py
@@ -1,1 +1,17 @@
+visited = []
+queue = []
 
+graph = {"A": ["B", "C"], "B": ["D"], "C": ["E"], "D": [], "E": []}
+start_node = "A"
+
+queue.append(start_node)
+visited.append(start_node)
+
+while queue:
+    current_node = queue.pop(0)
+    print(current_node)
+
+    for neighbor in graph[current_node]:
+        if neighbor not in visited:
+            visited.append(neighbor)
+            queue.append(neighbor)
